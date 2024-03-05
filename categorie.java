6d0bf3c7fb118830fc3d4f5f6ed6083d003ed1c2
HomosCristian <146970235+HomosCristian@users.noreply.github.com>
2024-03-05 08:01:48 +0200

    Add files via upload

---
+++ categorie.java
@@ -0,0 +1,12 @@
+package seminar.seminar3;
+
+public enum categorie {
+    TERENURI (211), CONSTRUCTII(212), ECHIPAMENTE_UTILAJE(2131), MIJLOACE_TRANSPORT(2133), MOBILIER(214);
+
+    private int simbol;
+    categorie(int simbol){
+        this.simbol = simbol;
+    }
+
+
+}
